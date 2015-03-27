db0a565c019e1383cb23d92b6a23961111ef2ffb
DSUK Paul Young <p.young@denso-sales.co.uk>
2015-03-27 14:14:14 +0000

    Update HMI to allow window and input/output areas to be resized

--- LuaInterpreter/Form1.Designer.cs
+++ LuaInterpreter/Form1.Designer.cs
@@ -29,54 +29,41 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
-			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.inputGroupBox = new System.Windows.Forms.GroupBox();
 			this.inputTextBox = new System.Windows.Forms.TextBox();
 			this.outputGroupBox = new System.Windows.Forms.GroupBox();
 			this.outputListBox = new System.Windows.Forms.ListBox();
 			this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
 			this.toolStripMenuItem_clear = new System.Windows.Forms.ToolStripMenuItem();
-			this.panel1 = new System.Windows.Forms.Panel();
+			this.timeoutTextBox = new System.Windows.Forms.TextBox();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.timeoutLabel = new System.Windows.Forms.Label();
 			this.runButton = new System.Windows.Forms.Button();
 			this.openFileButton = new System.Windows.Forms.Button();
 			this.saveFileButton = new System.Windows.Forms.Button();
 			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
 			this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
-			this.timeoutLabel = new System.Windows.Forms.Label();
-			this.timeoutTextBox = new System.Windows.Forms.TextBox();
 			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.tableLayoutPanel1.SuspendLayout();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
 			this.inputGroupBox.SuspendLayout();
 			this.outputGroupBox.SuspendLayout();
 			this.contextMenuStrip.SuspendLayout();
-			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// tableLayoutPanel1
-			// 
-			this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.tableLayoutPanel1.ColumnCount = 1;
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel1.Controls.Add(this.inputGroupBox, 0, 1);
-			this.tableLayoutPanel1.Controls.Add(this.outputGroupBox, 0, 2);
-			this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
-			this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
-			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-			this.tableLayoutPanel1.RowCount = 3;
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.41688F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.58312F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 130F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(678, 495);
-			this.tableLayoutPanel1.TabIndex = 0;
 			// 
 			// inputGroupBox
 			// 
 			this.inputGroupBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.inputGroupBox.Controls.Add(this.inputTextBox);
-			this.inputGroupBox.Location = new System.Drawing.Point(3, 30);
+			this.inputGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.inputGroupBox.Location = new System.Drawing.Point(0, 0);
 			this.inputGroupBox.Name = "inputGroupBox";
-			this.inputGroupBox.Size = new System.Drawing.Size(672, 331);
+			this.inputGroupBox.Size = new System.Drawing.Size(584, 310);
 			this.inputGroupBox.TabIndex = 1;
 			this.inputGroupBox.TabStop = false;
 			this.inputGroupBox.Text = "Input";
@@ -85,13 +72,14 @@
 			// 
 			this.inputTextBox.AcceptsReturn = true;
 			this.inputTextBox.AcceptsTab = true;
+			this.inputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.inputTextBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.inputTextBox.Location = new System.Drawing.Point(6, 19);
+			this.inputTextBox.Location = new System.Drawing.Point(3, 16);
 			this.inputTextBox.MaxLength = 1677215;
 			this.inputTextBox.Multiline = true;
 			this.inputTextBox.Name = "inputTextBox";
 			this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.inputTextBox.Size = new System.Drawing.Size(660, 306);
+			this.inputTextBox.Size = new System.Drawing.Size(578, 291);
 			this.inputTextBox.TabIndex = 0;
 			this.inputTextBox.Text = "-- defines a factorial function\r\nfunction fact (n)\r\n\tif (n == 0) then\r\n\t\treturn 1" +
     "\r\n\telse\r\n\t\treturn n*fact(n - 1)\r\n\tend\r\nend\r\n\r\nreturn fact(5)";
@@ -101,9 +89,10 @@
 			// 
 			this.outputGroupBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.outputGroupBox.Controls.Add(this.outputListBox);
-			this.outputGroupBox.Location = new System.Drawing.Point(3, 367);
+			this.outputGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.outputGroupBox.Location = new System.Drawing.Point(0, 0);
 			this.outputGroupBox.Name = "outputGroupBox";
-			this.outputGroupBox.Size = new System.Drawing.Size(672, 125);
+			this.outputGroupBox.Size = new System.Drawing.Size(584, 75);
 			this.outputGroupBox.TabIndex = 2;
 			this.outputGroupBox.TabStop = false;
 			this.outputGroupBox.Text = "Output";
@@ -111,12 +100,13 @@
 			// outputListBox
 			// 
 			this.outputListBox.ContextMenuStrip = this.contextMenuStrip;
+			this.outputListBox.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.outputListBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.outputListBox.FormattingEnabled = true;
 			this.outputListBox.ItemHeight = 16;
-			this.outputListBox.Location = new System.Drawing.Point(6, 19);
+			this.outputListBox.Location = new System.Drawing.Point(3, 16);
 			this.outputListBox.Name = "outputListBox";
-			this.outputListBox.Size = new System.Drawing.Size(660, 100);
+			this.outputListBox.Size = new System.Drawing.Size(578, 56);
 			this.outputListBox.TabIndex = 0;
 			// 
 			// contextMenuStrip
@@ -133,23 +123,40 @@
 			this.toolStripMenuItem_clear.Text = "Clear";
 			this.toolStripMenuItem_clear.Click += new System.EventHandler(this.toolStripMenuItem_clear_Click);
 			// 
-			// panel1
+			// timeoutTextBox
 			// 
-			this.panel1.Controls.Add(this.timeoutTextBox);
-			this.panel1.Controls.Add(this.timeoutLabel);
-			this.panel1.Controls.Add(this.runButton);
-			this.panel1.Controls.Add(this.openFileButton);
-			this.panel1.Controls.Add(this.saveFileButton);
-			this.panel1.Location = new System.Drawing.Point(3, 3);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(672, 21);
-			this.panel1.TabIndex = 3;
+			this.timeoutTextBox.ContextMenuStrip = this.contextMenuStrip1;
+			this.timeoutTextBox.Location = new System.Drawing.Point(132, 2);
+			this.timeoutTextBox.MinimumSize = new System.Drawing.Size(0, 20);
+			this.timeoutTextBox.Name = "timeoutTextBox";
+			this.timeoutTextBox.Size = new System.Drawing.Size(100, 20);
+			this.timeoutTextBox.TabIndex = 4;
+			this.timeoutTextBox.Text = "500";
+			this.toolTip1.SetToolTip(this.timeoutTextBox, "Nominal timout in millisceconds\r\n\r\nHigh processing (especially text output) will " +
+        "skew the actual timout period.");
+			this.timeoutTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.timeoutTextBox_KeyPress);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			// 
+			// timeoutLabel
+			// 
+			this.timeoutLabel.AutoSize = true;
+			this.timeoutLabel.Location = new System.Drawing.Point(81, 5);
+			this.timeoutLabel.Name = "timeoutLabel";
+			this.timeoutLabel.Size = new System.Drawing.Size(45, 13);
+			this.timeoutLabel.TabIndex = 3;
+			this.timeoutLabel.Text = "Timeout";
+			this.timeoutLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// runButton
 			// 
-			this.runButton.Location = new System.Drawing.Point(3, 0);
+			this.runButton.Dock = System.Windows.Forms.DockStyle.Left;
+			this.runButton.Location = new System.Drawing.Point(0, 0);
 			this.runButton.Name = "runButton";
-			this.runButton.Size = new System.Drawing.Size(75, 21);
+			this.runButton.Size = new System.Drawing.Size(75, 23);
 			this.runButton.TabIndex = 0;
 			this.runButton.Text = "Run Code";
 			this.runButton.UseVisualStyleBackColor = true;
@@ -157,7 +164,8 @@
 			// 
 			// openFileButton
 			// 
-			this.openFileButton.Location = new System.Drawing.Point(513, -1);
+			this.openFileButton.Dock = System.Windows.Forms.DockStyle.Right;
+			this.openFileButton.Location = new System.Drawing.Point(509, 0);
 			this.openFileButton.Name = "openFileButton";
 			this.openFileButton.Size = new System.Drawing.Size(75, 23);
 			this.openFileButton.TabIndex = 1;
@@ -167,7 +175,8 @@
 			// 
 			// saveFileButton
 			// 
-			this.saveFileButton.Location = new System.Drawing.Point(594, -1);
+			this.saveFileButton.Dock = System.Windows.Forms.DockStyle.Right;
+			this.saveFileButton.Location = new System.Drawing.Point(434, 0);
 			this.saveFileButton.Name = "saveFileButton";
 			this.saveFileButton.Size = new System.Drawing.Size(75, 23);
 			this.saveFileButton.TabIndex = 2;
@@ -185,57 +194,65 @@
 			this.saveFileDialog.DefaultExt = "lua";
 			this.saveFileDialog.Filter = "Lua files|*.lua|All files|*.*";
 			// 
-			// timeoutLabel
+			// panel2
 			// 
-			this.timeoutLabel.AutoSize = true;
-			this.timeoutLabel.Location = new System.Drawing.Point(130, 4);
-			this.timeoutLabel.Name = "timeoutLabel";
-			this.timeoutLabel.Size = new System.Drawing.Size(45, 13);
-			this.timeoutLabel.TabIndex = 3;
-			this.timeoutLabel.Text = "Timeout";
+			this.panel2.Controls.Add(this.timeoutTextBox);
+			this.panel2.Controls.Add(this.saveFileButton);
+			this.panel2.Controls.Add(this.timeoutLabel);
+			this.panel2.Controls.Add(this.openFileButton);
+			this.panel2.Controls.Add(this.runButton);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel2.Location = new System.Drawing.Point(0, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(584, 23);
+			this.panel2.TabIndex = 2;
 			// 
-			// timeoutTextBox
+			// splitContainer1
 			// 
-			this.timeoutTextBox.ContextMenuStrip = this.contextMenuStrip1;
-			this.timeoutTextBox.Location = new System.Drawing.Point(181, 1);
-			this.timeoutTextBox.Name = "timeoutTextBox";
-			this.timeoutTextBox.Size = new System.Drawing.Size(100, 20);
-			this.timeoutTextBox.TabIndex = 4;
-			this.timeoutTextBox.Text = "500";
-			this.toolTip1.SetToolTip(this.timeoutTextBox, "Nominal timout in millisceconds\r\n\r\nHigh processing (especially text output) will " +
-        "skew the actual timout period.");
-			this.timeoutTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.timeoutTextBox_KeyPress);
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 23);
+			this.splitContainer1.MinimumSize = new System.Drawing.Size(0, 100);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
 			// 
-			// contextMenuStrip1
+			// splitContainer1.Panel1
 			// 
-			this.contextMenuStrip1.Name = "contextMenuStrip1";
-			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			this.splitContainer1.Panel1.Controls.Add(this.inputGroupBox);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.outputGroupBox);
+			this.splitContainer1.Panel2MinSize = 75;
+			this.splitContainer1.Size = new System.Drawing.Size(584, 389);
+			this.splitContainer1.SplitterDistance = 310;
+			this.splitContainer1.TabIndex = 3;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.ClientSize = new System.Drawing.Size(702, 519);
-			this.Controls.Add(this.tableLayoutPanel1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-			this.MaximizeBox = false;
+			this.ClientSize = new System.Drawing.Size(584, 412);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.panel2);
+			this.MinimumSize = new System.Drawing.Size(420, 250);
 			this.Name = "Form1";
 			this.Text = "Lua Interpreter";
-			this.tableLayoutPanel1.ResumeLayout(false);
 			this.inputGroupBox.ResumeLayout(false);
 			this.inputGroupBox.PerformLayout();
 			this.outputGroupBox.ResumeLayout(false);
 			this.contextMenuStrip.ResumeLayout(false);
-			this.panel1.ResumeLayout(false);
-			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
 		private System.Windows.Forms.Button runButton;
 		private System.Windows.Forms.GroupBox inputGroupBox;
 		private System.Windows.Forms.GroupBox outputGroupBox;
@@ -247,11 +264,12 @@
 		private System.Windows.Forms.SaveFileDialog saveFileDialog;
 		private System.Windows.Forms.Button openFileButton;
 		private System.Windows.Forms.Button saveFileButton;
-		private System.Windows.Forms.Panel panel1;
 		private System.Windows.Forms.TextBox timeoutTextBox;
 		private System.Windows.Forms.ToolTip toolTip1;
 		private System.Windows.Forms.Label timeoutLabel;
 		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.SplitContainer splitContainer1;
 
 	}
 }
